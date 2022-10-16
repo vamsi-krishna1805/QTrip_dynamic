ce87d53a96db8a2b4244efd187afc064336a2873
CrioUser <crio-user@criodo.com>
2022-10-16 16:19:16 +0530

    “ME_QTRIPDYNAMIC_MODULE_CITIES”

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,4 @@
 
-const config = { backendEndpoint: "http://15.207.168.169:8082" };
+const config = { backendEndpoint: "http://3.110.17.71:8082" };
 
 export default config;
--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -13,25 +13,37 @@ async function init() {
 
 //Implementation of fetch call
 async function fetchCities() {
+
   // TODO: MODULE_CITIES
   // 1. Fetch cities using the Backend API and return the data
-const res=await fetch('http://15.207.168.169:8082/cities');
-const cities=await res.json();
-return cities;
+try{
+  const res=await fetch(config.backendEndpoint+"/cities");
+  const data=await res.json();
+  return data;
+}catch{
+  return (null); 
+}
 }
 
 //Implementation of DOM manipulation to add cities
 function addCityToDOM(id, city, description, image) {
   // TODO: MODULE_CITIES
   // 1. Populate the City details and insert those details into the DOM
-  let divEle=document.createElement("div");
-  divEle.className='col-6 col-lg-3 mb-3';
-  let innerHTML=`<a id=${id} href="pages/adventures/?city=${id} "><img href="${image}" ><h5>${city}</h5><p>${description}</p></a>`;
-  
-  divEle.innerHTML=innerHTML;
-  
-  let contain=document.getElementById("data").append(divEle);
-  return contain;
+  const dataEle=document.getElementById("data");
+  dataEle.innerHTML+=`
+  <div class="col-6 col-lg-3 mb-4">
+  <a href="pages/adventures/?city=${id}" id="${id}">
+    <div class="tile">
+      <div class="tile-text text-center">
+        <h5>${city}</h5>
+        <p>${description}</p>
+      </div>
+      <img class="img-responsive" src="${image}">
+    </div>
+  </a>
+</div>
+`
+return dataEle;
 
 }
 
